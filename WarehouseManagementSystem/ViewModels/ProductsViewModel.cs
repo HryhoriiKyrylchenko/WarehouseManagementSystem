78f3d8a8ad75f37c5e9b8ba40d7bf3c6df84ba77
Hryhorii Kyrylchenko <gregorykirilchenko@gmail.com>
2023-12-23 20:11:39 +0100

    Added View and ViewModel classes for displaying application pages. Added class WarehouseDBManager for working with database.

--- WarehouseManagementSystem/ViewModels/ProductsViewModel.cs
+++ WarehouseManagementSystem/ViewModels/ProductsViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using WarehouseManagementSystem.Commands;
+using WarehouseManagementSystem.Models;
+using WarehouseManagementSystem.Services;
 
 namespace WarehouseManagementSystem.ViewModels
 {
@@ -12,9 +15,33 @@ namespace WarehouseManagementSystem.ViewModels
     {
         private readonly MainViewModel mainViewModel;
 
+        public ObservableCollection<CategoryViewModel> Categories { get; set; }
+
         public ProductsViewModel(MainViewModel mainViewModel)
         {
             this.mainViewModel = mainViewModel;
+            Categories = new ObservableCollection<CategoryViewModel>();
+
+            InitializeAsync();
+        }
+
+        private async void InitializeAsync()
+        {
+            await InitializwCategoriesFromDBAsync();
+        }
+
+        private async Task InitializwCategoriesFromDBAsync()
+        {
+            using (var dbManager = new WarehouseDBManager(new WarehouseDbContext()))
+            {
+                var rootCategories = await dbManager.GetRootCategoriesAsync();
+
+                foreach (var rootCategory in rootCategories)
+                {
+                    var rootViewModel = await dbManager.BuildCategoryViewModelTreeAsync(rootCategory);
+                    Categories.Add(rootViewModel);
+                }
+            }           
         }
 
         public ICommand BackCommand => new RelayCommand(Back);
