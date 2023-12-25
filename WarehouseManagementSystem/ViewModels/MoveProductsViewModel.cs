685e1aa3c22f7c37203fc2a1c6b304e23425659f
Hryhorii Kyrylchenko <gregorykirilchenko@gmail.com>
2023-12-25 16:34:18 +0100

    New View and ViewModel classes have been added, changes have been made to existing ones.

---
+++ WarehouseManagementSystem/ViewModels/MoveProductsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WarehouseManagementSystem.Commands;
+using WarehouseManagementSystem.Models;
+using WarehouseManagementSystem.Models.Entities;
+using WarehouseManagementSystem.Services;
+
+namespace WarehouseManagementSystem.ViewModels
+{
+    public class MoveProductsViewModel : ViewModelBase
+    {
+        private readonly MainViewModel mainViewModel;
+
+        public MoveProductsViewModel(MainViewModel mainViewModel) 
+        {
+            this.mainViewModel = mainViewModel;
+        }
+
+        public ICommand BackCommand => new RelayCommand(Back);
+
+        private void Back(object parameter)
+        {
+            mainViewModel.NavigateBack();
+        }
+    }
+}
