4e65706a8ac3b311df9629f7ef42ad12f9b93341
Hryhorii Kyrylchenko <gregorykirilchenko@gmail.com>
2024-01-19 00:59:50 +0100

    The logging process has been rewritten using the MVVM pattern.

---
+++ WarehouseManagementSystem/ViewModels/Helpers/PasswordBoxHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace WarehouseManagementSystem.ViewModels.Helpers
+{
+    public class PasswordBoxHelper : DependencyObject
+    {
+        public static readonly DependencyProperty BoundPasswordProperty =
+            DependencyProperty.RegisterAttached("BoundPassword", typeof(string), typeof(PasswordBoxHelper), new PropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+        public static readonly DependencyProperty BindPasswordProperty =
+            DependencyProperty.RegisterAttached("BindPassword", typeof(bool), typeof(PasswordBoxHelper), new PropertyMetadata(false, OnBindPasswordChanged));
+
+        private static readonly DependencyProperty UpdatingPasswordProperty =
+            DependencyProperty.RegisterAttached("UpdatingPassword", typeof(bool), typeof(PasswordBoxHelper), new PropertyMetadata(false));
+
+        public static string GetBoundPassword(DependencyObject d)
+        {
+            return (string)d.GetValue(BoundPasswordProperty);
+        }
+
+        public static void SetBoundPassword(DependencyObject d, string value)
+        {
+            d.SetValue(BoundPasswordProperty, value);
+        }
+
+        public static bool GetBindPassword(DependencyObject d)
+        {
+            return (bool)d.GetValue(BindPasswordProperty);
+        }
+
+        public static void SetBindPassword(DependencyObject d, bool value)
+        {
+            d.SetValue(BindPasswordProperty, value);
+        }
+
+        private static bool GetUpdatingPassword(DependencyObject d)
+        {
+            return (bool)d.GetValue(UpdatingPasswordProperty);
+        }
+
+        private static void SetUpdatingPassword(DependencyObject d, bool value)
+        {
+            d.SetValue(UpdatingPasswordProperty, value);
+        }
+
+        private static void OnBoundPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var passwordBox = (PasswordBox)d;
+            passwordBox.PasswordChanged -= PasswordChanged;
+
+            if (!(bool)GetUpdatingPassword(passwordBox))
+            {
+                passwordBox.Password = (string)e.NewValue;
+            }
+
+            passwordBox.PasswordChanged += PasswordChanged;
+        }
+
+        private static void OnBindPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var passwordBox = (PasswordBox)d;
+            passwordBox.PasswordChanged -= PasswordChanged;
+
+            if ((bool)e.NewValue)
+            {
+                SetUpdatingPassword(passwordBox, true);
+                SetBoundPassword(passwordBox, passwordBox.Password);
+                passwordBox.PasswordChanged += PasswordChanged;
+            }
+        }
+
+        private static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var passwordBox = (PasswordBox)sender;
+            SetUpdatingPassword(passwordBox, true);
+            SetBoundPassword(passwordBox, passwordBox.Password);
+            SetUpdatingPassword(passwordBox, false);
+        }
+    }
+}
